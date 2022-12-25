469cfe2d75bcb19b0e03d7fa3f3d49e68ec4d367
Home <ildar86@live.ru>
2022-12-25 17:29:32 +0500

    заполнен класс TaskService

--- src/DailyPlanner/Task.java
+++ src/DailyPlanner/Task.java
@@ -4,19 +4,24 @@ import java.time.*;
 import java.util.*;
 
 public class Task {
-    private String heading;
-    private String description;
-    private final TaskType taskType;
-    private LocalDateTime dateTime;
-    final private int id;
+
     private static int idCounter = 1;
 
-    public Task(String heading, String description, TaskType taskType, LocalDateTime dateTime) {
-        this.heading = heading;
-        this.description = description;
-        this.taskType = taskType;
-        this.dateTime = dateTime;
+    final private int id;
+    private String heading;
+    private String description;
+    private TaskType taskType;
+    private LocalDateTime dateTime;
+    Repeatability repeatability;
+
+    public Task(String heading, String description, TaskType taskType,
+                LocalDateTime dateTime, Repeatability repeatability) {
         id = idCounter++;
+        setHeading(heading);
+        setDescription(description);
+        setTaskType(taskType);
+        setDateTime(dateTime);
+        setRepeatability(repeatability);
     }
 
     public String getHeading() {
@@ -24,6 +29,9 @@ public class Task {
     }
 
     public void setHeading(String heading) {
+        if (StringUtils.isNullOrEmpty(heading)) {
+            throw new IllegalArgumentException("не заполнен заголовок задачи!");
+        }
         this.heading = heading;
     }
 
@@ -32,6 +40,9 @@ public class Task {
     }
 
     public void setDescription(String description) {
+        if (StringUtils.isNullOrEmpty(description)) {
+            throw new IllegalArgumentException("заполните описание задачи!");
+        }
         this.description = description;
     }
 
@@ -39,16 +50,37 @@ public class Task {
         return taskType;
     }
 
+    public void setTaskType(TaskType taskType) {
+        if (taskType == null) {
+            taskType = TaskType.PERSONAL;
+        }
+        this.taskType = taskType;
+    }
+
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
     public void setDateTime(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            throw new IllegalArgumentException("введите верно дату и время задачи");
+        }
         this.dateTime = dateTime;
     }
 
     public int getId() {
         return id;
+    }
+
+    public Repeatability getRepeatability() {
+        return repeatability;
+    }
+
+    public void setRepeatability(Repeatability repeatability) {
+        if (repeatability == null) {
+            repeatability = new OneTime();
+        }
+        this.repeatability = repeatability;
     }
 
     @Override
--- src/DailyPlanner/TaskService.java
+++ src/DailyPlanner/TaskService.java
@@ -1,12 +1,50 @@
 package DailyPlanner;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class TaskService {
-    public static final Map<Integer, Task> TASKS = new HashMap<>();
+    private static final Map<Integer, Task> TASKS = new HashMap<>();
 
-    public TaskService() {
+    private TaskService() {
+    }
+
+    public static void add(Task task) {
+        TASKS.put(task.getId(), task);
+    }
+
+    public static Collection<Task> getTasksForDay(LocalDate date) {
+        Collection<Task> tasksForDay = new LinkedList<>();
+        Collection<Task> allTasks = TASKS.values();
+        for (Task task : allTasks) {
+            LocalDateTime currentDateTime = task.getDateTime();
+            if (currentDateTime.toLocalDate().equals(date)) {
+                tasksForDay.add(task);
+                continue;
+            }
+            LocalDateTime nextDateTime = currentDateTime;
+            do {
+                nextDateTime = task.getRepeatability().nextTime(nextDateTime);
+                if (nextDateTime == null) {
+                    break;
+                }
+                if (nextDateTime.toLocalDate().equals(date)) {
+                    tasksForDay.add(task);
+                    break;
+                }
+            } while (nextDateTime.toLocalDate().isBefore(date));
+        }
+        return tasksForDay;
+    }
+
+    public static void deleteTaskById(int id) throws IdNotFoundException {
+        if (TASKS.remove(id) == null) {
+            throw new IdNotFoundException("запрашиваемый id не найден");
+        }
     }
 
 }
